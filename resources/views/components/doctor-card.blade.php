ea7b6196cc4fcc96b4e3b21aa04736ddfd4bf308
ikromr04 <ikromr04@gmail.com>
2024-11-11 16:19:12 +0500

    Makes doctors block

--- resources/views/components/doctor-card.blade.php
+++ resources/views/components/doctor-card.blade.php
@@ -8,7 +8,6 @@
     }
   </style>
   <div class="hidden md:block pointer-events-none absolute -top-[5px] left-0 h-10 rounded-[10px] bg-brand w-[120px] -z-10"></div>
-  {{-- <div class="hidden md:block pointer-events-none absolute top-0 left-0 w-full h-10 bg-white rounded-[10px]"></div> --}}
 
   <a class="relative doctor-avatar flex border border-brand rounded-[10px] w-[240px] h-[240px] overflow-hidden mb-5 md:h-[360px] md:w-full" href="{{ route('doctor', $doctor->slug) }}">
     <img class="absolute w-full h-full object-cover object-top transition-all origin-top duration-300 hover:scale-125 bg-slate-200" src="{{ asset("/images/doctors/$doctor->avatar") }}" width="240" height="360" alt="{{ $doctor->name }}">
