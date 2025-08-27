40291ba153b2f570796825806377db0bec324446
ikromr04 <ikromr04@gmail.com>
2025-08-27 15:46:30 +0500

    Bug fix

--- resources/views/components/category-card.blade.php
+++ resources/views/components/category-card.blade.php
@@ -8,10 +8,10 @@
   <div class="py-8 pl-6 pr-10">
     <h3 class="font-semibold mb-3 flex items-center gap-x-2 uppercase">
       <span class="flex items-center justify-center text-white min-w-12 min-h-12 bg-brand rounded-full max-h-12 max-w-12 leading-none font-semibold text-[29px]">0{{ ++$key }}</span>
-      {{ $category->title }}
+      {!! $category->title !!}
     </h3>
 
-    <p>{{ $category->description }}</p>
+    <p>{!! $category->description !!}</p>
 
     <a class="text-sm flex font-semibold max-w-max mt-3" href="{{ route('category', $category->slug) }}">
       {{ __('Все статьи') }}
