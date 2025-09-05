b0fab5d61b64726422441f4c9011918042433742
ikromr04 <ikromr04@gmail.com>
2025-09-05 18:06:01 +0500

    Add indexation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,24 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="robots" content="noindex, nofollow">
+
+  <meta name="description" content="Farna Euro Clinic – европейский уровень медицины в Таджикистане. Диагностика, лечение неврологических заболеваний, консультации специалистов. Запишитесь на прием онлайн.">
+  <meta name="keywords" content="неврология Душанбе, клиника Душанбе, Farna Euro Clinic, лечение, диагностика, медицина Таджикистан">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Language" content="ru">
+
+  <meta property="og:title" content="Farna Euro Clinic – Современная медицинская клиника в Душанбе">
+  <meta property="og:description" content="Европейский уровень медицины в Таджикистане. Современное оборудование, опытные врачи, диагностика и лечение.">
+  <meta property="og:image" content="https://farnaeuroclinic.tj/images/og-image.jpg">
+  <meta property="og:url" content="https://farnaeuroclinic.tj/">
+  <meta property="og:type" content="website">
+
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="Farna Euro Clinic – Современная клиника в Душанбе">
+  <meta name="twitter:description" content="Диагностика и лечение неврологических заболеваний в Таджикистане. Запишитесь на прием онлайн.">
+  <meta name="twitter:image" content="https://farnaeuroclinic.tj/images/og-image.jpg">
+
+  <link rel="canonical" href="https://farnaeuroclinic.tj/">
 
   <link rel="icon" href="{{ asset('favicon.ico') }}">
   <link rel="icon" href="{{ asset('favicons/icon.svg') }}" type="image/svg+xml">
@@ -19,6 +36,48 @@
   @vite(['resources/css/app.css'])
 
   <title>@yield('title'){{ config('app.name') }}</title>
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XKXGF9M8RW"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-XKXGF9M8RW');
+  </script>
+
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript">
+    (function(m, e, t, r, i, k, a) {
+      m[i] = m[i] || function() {
+        (m[i].a = m[i].a || []).push(arguments)
+      };
+      m[i].l = 1 * new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) {
+          return;
+        }
+      }
+      k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=104044726', 'ym');
+
+    ym(104044726, 'init', {
+      ssr: true,
+      webvisor: true,
+      clickmap: true,
+      ecommerce: "dataLayer",
+      accurateTrackBounce: true,
+      trackLinks: true
+    });
+  </script>
+  <noscript>
+    <div><img src="https://mc.yandex.ru/watch/104044726" style="position:absolute; left:-9999px;" alt="" /></div>
+  </noscript>
+  <!-- /Yandex.Metrika counter -->
 </head>
 
 <body class="flex flex-col min-h-screen group">
